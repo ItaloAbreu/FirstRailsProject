eef42983761a51268ffa2beb4228ba6e244a8512
Italo Abreu <italodw@gmail.com>
2019-08-30 17:38:02 -0300

    Saving data in the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,13 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-
+       
         @article.save
         redirect_to @article
     end
-
+       
     private
         def article_params
-            params.require[:article].permit(:title, :text)
+            params.require(:article).permit(:title, :text)
         end
 end
